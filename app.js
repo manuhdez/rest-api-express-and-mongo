9f42071bda63f089f08147423de1b4c5371dc4aa
Manuel Hernández <manu.hdez.dev@gmail.com>
2018-08-22 19:44:39 +0100

    Using Middleware in Express

--- app.js
+++ app.js
@@ -3,7 +3,12 @@
 const express = require('express');
 const app = express();
 
-
+// Middleware functions
+app.use((req, res, next) => {
+  // Do something
+  console.log(`the leaves on the trees are ${req.query.color}`);
+  next();
+});
 
 // Server listener
 const port = process.env.PORT || 3000;
