c63c5255e67214445743e4b731cc80bd0f1ef701
Manuel Hernández <manu.hdez.dev@gmail.com>
2018-08-23 19:22:37 +0100

    Default Values in Mongoose

--- mongoose_sandbox.js
+++ mongoose_sandbox.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 
 // Conect mongoose to a database inside mongo
-mongoose.connect('mongodb://localhost:27017/sandbox');
+mongoose.connect('mongodb://localhost:27017/sandbox', {useNewUrlParser: true});
 
 // create a db object to listen for mongoose conection events
 const db = mongoose.connection;
@@ -18,11 +18,11 @@ db.once('open', () => {
 
   const Schema = mongoose.Schema;
   const AnimalSchema = new Schema({
-    type: String,
-    size: String,
-    color: String,
-    mass: Number,
-    name: String
+    type: {type: String, default: "goldfish"},
+    size: {type: String, default: "small"},
+    color: {type: String, default: "golden"},
+    mass: {type: Number, default: 0.007},
+    name: {type: String, default: "Angela"}
   });
 
   const Animal = mongoose.model("Animal", AnimalSchema);
@@ -35,13 +35,36 @@ db.once('open', () => {
     name: 'Dumbo'
   });
 
-  elephant.save((err) => {
-    if (err) console.error("Save Failed!", err);
-    else console.log("Saved!");
+  const whale = new Animal({
+    type: "whale",
+    size: "big",
+    mass: 190500,
+    name: "Fig"
+  });
 
-    db.close(() => {
-      console.log('Connection closed.');
+  const animal = new Animal({});
+
+  // Empty animals collection before saving
+  Animal.deleteMany({}, (err) => {
+    if (err) console.error(err);
+    // Save animals in the collection after the collection its empty
+    elephant.save((err) => {
+      if (err) console.error("Save Failed!", err);
+      animal.save((err) => {
+        if (err) console.error("Save Failed!", err);
+        whale.save((err) => {
+          if (err) console.error("Save Failed!", err);
+          // Read all the animals inside the collection that has a "big" size
+          Animal.find({size: "big"}, (err, animals) => {
+            if (err) console.error(err);
+            animals.forEach( animal => console.log(`${animal.name} the ${animal.color} ${animal.type}`));
+            db.close(() => {
+              console.log('Connection closed.');
+            });
+          });
+        });
+      });
     });
   });
 
-})
+});
