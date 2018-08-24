8ac1be9fb63fbc161b0dc8abb93e89690894d51a
Manuel Hernández <manu.hdez.dev@gmail.com>
2018-08-24 17:57:17 +0100

    Question and Answer Schemas

--- app.js
+++ app.js
@@ -5,10 +5,26 @@ const app = express();
 const routes = require('./routes');
 const jsonParser = require('body-parser').json;
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 // Middleware functions
 app.use(logger("dev"));
 app.use(jsonParser());
+
+// Mongoose
+mongoose.connect('mongodb://localhost:27017/qa', {useNewUrlParser: true});
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+  console.error('connection error', err);
+});
+
+db.once('open', () => {
+  console.log('Conected succesfully');
+  // all database comunication goes here
+});
+
+// Routes
 app.use('/questions', routes);
 
 // catch 404 and forward to error handler
